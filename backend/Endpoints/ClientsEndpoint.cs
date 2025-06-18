1bbea67974e6685ad1ec42c62f854c2f6fc1124b
Fedor Khamitov <93128664+fedorkhamitov@users.noreply.github.com>
2025-06-18 15:16:29 +0300

    Add files via upload

---
+++ backend/Endpoints/ClientsEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using AdminDashboard.Entities;
+using AdminDashboard.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.Endpoints;
+
+public static class ClientsEndpoint
+{
+    public static async Task<IEnumerable<Client>> GetClients(AppDbContext db)
+    {
+        return await db.Clients.ToListAsync();
+    }
+}
---
+++ backend/Endpoints/PaymentsEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using AdminDashboard.Entities;
+using AdminDashboard.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.Endpoints;
+
+public static class PaymentsEndpoint
+{
+    public static async Task<IEnumerable<Payment>> GetPayments(AppDbContext db, int take = 5)
+    {
+        return await db.Payments
+            .OrderByDescending(p => p.Date)
+            .Take(take)
+            .ToListAsync();
+    }
+}
